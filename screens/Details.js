bb3aff6b3239a91d6c37b4f5065980287bb5c1b5
Aaryan-kapur <44099242+Aaryan-kapur@users.noreply.github.com>
2019-12-10 01:59:37 +0530

    edit-_match_history

--- screens/Details.js
+++ screens/Details.js
@@ -7,7 +7,8 @@ class Details extends React.Component {
   state = {};
   render() {
     return (
-      <View>
+      <View >
+          <View style={{ backgroundColor: "ghostwhite" }}>
         <View style={{ alignItems: "center", justifyContent: "center" }}>
           <Header
             leftComponent={{ icon: "home", color: "#fff" }}
@@ -17,7 +18,7 @@ class Details extends React.Component {
             }}
           />
         </View>
-        <View style={{ alignItems:"center"}}>
+        <View style={{ alignItems: "center" }}>
           <Text style={{ fontWeight: "bold", fontSize: 30 }}>
             Match History
           </Text>
@@ -52,64 +53,7 @@ class Details extends React.Component {
             rightAvatar={{ rounded: true }}
             leftElementStyle={{ color: "white", fontWeight: "bold" }}
           />
-          <Text style={{ marginTop: 10, fontWeight: "bold" }}>Match A</Text>
-          <ListItem
-            style={{ marginTop: 10 }}
-            Component={TouchableScale}
-            friction={90} //
-            tension={100} // These props are passed to the parent component (here TouchableScale)
-            activeScale={0.95} //
-            linearGradientProps={{
-              colors: ["#12c2e9", "#4A00E0"],
-              start: [1, 0],
-              end: [0.2, 0]
-            }}
-            ViewComponent={LinearGradient} // Only if no expo
-            leftAvatar={{ rounded: true }}
-            title="0"
-            subtitle="Team A"
-            titleStyle={{ color: "white", fontWeight: "bold", fontSize: 40 }}
-            subtitleStyle={{ color: "white", fontWeight: "bold" }}
-            rightTitle="0"
-            rightSubtitle="Team B"
-            rightTitleStyle={{
-              color: "white",
-              fontWeight: "bold",
-              fontSize: 40
-            }}
-            rightSubtitleStyle={{ color: "white", fontWeight: "bold" }}
-            rightAvatar={{ rounded: true }}
-            leftElementStyle={{ color: "white", fontWeight: "bold" }}
-          />
-          <Text style={{ marginTop: 10, fontWeight: "bold" }}>Match A</Text>
-          <ListItem
-            style={{ marginTop: 10 }}
-            Component={TouchableScale}
-            friction={90} //
-            tension={100} // These props are passed to the parent component (here TouchableScale)
-            activeScale={0.95} //
-            linearGradientProps={{
-              colors: ["#bc4e9c", "#f80759"],
-              start: [1, 0],
-              end: [0.2, 0]
-            }}
-            ViewComponent={LinearGradient} // Only if no expo
-            leftAvatar={{ rounded: true }}
-            title="0"
-            subtitle="Team A"
-            titleStyle={{ color: "white", fontWeight: "bold", fontSize: 40 }}
-            subtitleStyle={{ color: "white", fontWeight: "bold" }}
-            rightTitle="0"
-            rightSubtitle="Team B"
-            rightTitleStyle={{
-              color: "white",
-              fontWeight: "bold",
-              fontSize: 40
-            }}
-            rightSubtitleStyle={{ color: "white", fontWeight: "bold" }}
-            rightAvatar={{ rounded: true }}
-            leftElementStyle={{ color: "white", fontWeight: "bold" }}
-          />
+
           <Text style={{ marginTop: 10, fontWeight: "bold" }}>Match A</Text>
           <ListItem
             style={{ marginTop: 10 }}
@@ -139,7 +83,37 @@ class Details extends React.Component {
             rightAvatar={{ rounded: true }}
             leftElementStyle={{ color: "white", fontWeight: "bold" }}
           />
+          <Text style={{ marginTop: 10, fontWeight: "bold" }}>Match A</Text>
+          <ListItem
+            style={{ marginTop: 10 }}
+            Component={TouchableScale}
+            friction={90} //
+            tension={100} // These props are passed to the parent component (here TouchableScale)
+            activeScale={0.95} //
+            linearGradientProps={{
+              colors: ["#FF9800", "#F44336"],
+              start: [1, 0],
+              end: [0.2, 0]
+            }}
+            ViewComponent={LinearGradient} // Only if no expo
+            leftAvatar={{ rounded: true }}
+            title="0"
+            subtitle="Team A"
+            titleStyle={{ color: "white", fontWeight: "bold", fontSize: 40 }}
+            subtitleStyle={{ color: "white", fontWeight: "bold" }}
+            rightTitle="0"
+            rightSubtitle="Team B"
+            rightTitleStyle={{
+              color: "white",
+              fontWeight: "bold",
+              fontSize: 40
+            }}
+            rightSubtitleStyle={{ color: "white", fontWeight: "bold" }}
+            rightAvatar={{ rounded: true }}
+            leftElementStyle={{ color: "white", fontWeight: "bold" }}
+          />
         </ScrollView>
+        </View>
       </View>
     );
   }
